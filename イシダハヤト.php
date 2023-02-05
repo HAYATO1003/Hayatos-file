9fc83e048c5b6a03ae73587474e04994eb725b6a
HAYATO1003 <124515553+HAYATO1003@users.noreply.github.com>
2023-02-06 01:10:48 +0900

    Add files via upload

---
+++ イシダハヤト.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+      <meta name="viewport" content="width=320, height=480, initial-scale=1.0, minimum-scale=1.0, maximum-scale=2.0, user-scalable=yes"><!-- for smartphone. ここは一旦、いじらなくてOKです。 -->
+      <meta charset="UTF-8">
+        <title>石田隼斗のプロフィール</title>
+
+    </head>
+
+    <body>
+
+
+    <!-- 見出し 第１レベル -->
+    <!--（氏名）の公開を避ける場合はイニシャルやニックネームでOK-->
+   <span style="font: size 100px;"><h1 class="midashi_1"> 《 石田のプロフィール 》</h1></span>
+
+
+    <!--自分を象徴するような画像を入れてみよう-->
+    <!--画像も同じフォルダにアップロードすること-->
+    　　<img src="file:///C:/Users/ishay/OneDrive/%E3%83%87%E3%82%B9%E3%82%AF%E3%83%88%E3%83%83%E3%83%97/%E5%B0%B1%E6%B4%BB/%E8%A8%BC%E6%98%8E%E5%86%99%E7%9C%9F/%E8%87%AA%E5%B7%B1%E7%B4%B9%E4%BB%8B%20%EF%BC%88%E6%AD%A3%E6%96%B9%E5%BD%A2%EF%BC%89.JPG" alt="学生実験での私" title="学生実験での私" width="320px" ><br>
+    　（画像についてひとこと：大学での実験の様子。中和滴定の実験を行っている。）
+
+    <hr>
+
+    <!-- 見出し 第２レベル ・以下、h2レベルで項目を色々書いていきましょう -->
+    <!-- 自己紹介で言いたい項目を見出しにいれて、文章をつくりましょう。項目名は編集OK。項目を足してもOK。 -->
+    <div Style="background-color:rgb(188, 242, 252)">
+    <span style="color:rgb(105, 47, 254);"><h2>石田とは？</h2></span>
+    <span style="color:mediumblue;"><h2>　＜広島大学大学院 統合生命科学研究科 生物工学プログラムに所属＞</h2></span>
+    　代謝変換制御学研究室に所属しています。専攻は微生物学です。<br>
+    <br>
+    　<strong><span style="color:crimson;">～CO2からモノづくり～</span></strong><br>
+    　　地球温暖化の原因であるCO2を資源として酢酸・エタノール・アセトンなどの有用物質の工業生産を目指している。<br>
+    <br>
+    　　<a href="https://metab.hiroshima-u.ac.jp/ja/home-j/"target="_blank"><img src="代謝変換制御学研究室ホームページ画像.jpg" alt="ホームページ画像" title="ホームページ画像" width="320px"></a>
+
+    <button onclick="location.href='https://metab.hiroshima-u.ac.jp/ja/home-j/'"target="_blank">代謝変換制御学研究室</button>
+
+    <span style="color:mediumblue;"><h2>　＜研究概要＞</h2></span>
+    　【ガス発酵による有用物質生産の実用株の育種】<br>
+    　ガス発酵は微生物を活用し、二酸化炭素からモノづくりをする革新的な技術である。<br>
+    　具体的には、地球温暖化の原因物質である二酸化炭素と次世代のエネルギーを担う水素を資源とし、お酢の主成分である酢酸やバイオ燃料の原料となるエタノールなどを製造できる。<br>
+    　しかし、この微生物は酸素があると生育できない。私は社会実装に向け、酸素耐性が向上した個体の取得を目指し、酸素耐性能の評価及び遺伝子組み換えを活用したメカニズムの解明を行っている。<br>
+    
+
+    <span style="color:mediumblue;"><h2>　＜趣味と特技＞</h2></span>
+    <ul>
+        <li>筋トレ（ベンチプレスMAX85 kg）</li>
+        <li>腸活（ヨーグルト、甘酒作れます）</li>
+        <li>将棋（３級）</li>
+        <li>ボウリング（ハイスコア225）</li>
+        <li>ウインタースポーツ（スキー、スノボ両方できます）</li>
+    </ul>
+    <br>
+    　同じ趣味がある方、お話ししましょう！<br>
+
+    　　<img src="file:///C:/Users/ishay/OneDrive/%E3%83%87%E3%82%B9%E3%82%AF%E3%83%88%E3%83%83%E3%83%97/%E5%B0%B1%E6%B4%BB/%E8%A8%BC%E6%98%8E%E5%86%99%E7%9C%9F/%E8%B6%A3%E5%91%B3%E5%86%99%E7%9C%9F.JPG" alt="趣味は発酵食品を食べること" title="趣味は発酵食品を食べること" width="250px" >
+    　　<img src="file:///C:/Users/ishay/OneDrive/%E3%83%87%E3%82%B9%E3%82%AF%E3%83%88%E3%83%83%E3%83%97/%E5%B0%B1%E6%B4%BB/%E8%A8%BC%E6%98%8E%E5%86%99%E7%9C%9F/%E8%87%AA%E5%B7%B1%E7%B4%B9%E4%BB%8B%20%E7%AD%8B%E3%83%88%E3%83%AC(%E6%AD%A3%E6%96%B9%E5%BD%A2).JPG" alt="趣味の筋トレの画像" title="趣味の筋トレの画像" width="200px" ><br> 
+
+    <!-- HTMLで出来る色々な表現を試す -->
+    <!-- ↑文字に意味を持たせることもできます。strong は太字。ググったりして色々と調べてみましょう -->
+    <!-- 例として今回は「リスト表示」を作ってみましょう。項目数が３で足りないなら足してもOKです -->
+    <!-- 番号をふるなら ol タグ、番号がいらないなら ul タグ で囲って、項目は li タグで括ります。 -->
+    <!-- ol ・ ul 片方でOK -->
+    <span style="color:mediumblue;"><h2>　＜強みと弱み３選＞</h2></span>
+    　<strong> 私の強み3選です！</strong><br>
+    <!-- ↑文字に意味を持たせることもできます。strong は強調。太字になります。 -->
+
+    <ol>
+        <li> 成長意欲……好奇心旺盛で何事にも挑戦します。</li>
+        <li> 俯瞰的な分析力……全体を見据え、計画を立てることができます。</li>
+        <li> バイタリティー……活発的に活動しています。</li>
+    </ol>
+    　<strong> 私の弱み3選です！</strong><br>
+    <ol>
+        <li> マイペース → 定期的に周囲の状況を把握するように心がけたいです。</li>
+        <li> 自分を優先してしまうこと → チームワークを大切にし、共に切磋琢磨できる環境を作りたいです。</li>
+        <li> 夜型（朝に弱い） → 朝はコーヒーを飲んで強制覚醒させています。</li>
+    </ol>
+    </div>
+    <hr>
+
+    <!-- 以下は、見出しの名前は固定で、内容を編集してみましょう -->
+    <div Style="background-color:peachpuff">
+        <span style="color:rgb(249, 112, 38);"><h2>その他</h2></span>
+    <!-- 見出し 第３レベル -->
+    <h3>使用PC</h3><!-- 編集して、自分の使うPCだけ残しましょう -->
+     Windows
+
+    <h3>PC利用経験や、普段の使い方</h3>
+    基本的に研究室で使用しています。ExcelやPowerPointにて研究の週刊報告の資料を作成しています。<br>
+    League of Legends（LOL）というPCゲームを暇なときにやります。<br>
+    <button onclick="location.href='https://www.leagueoflegends.com/ja-jp/'" target="_blank">League of Legends</button>
+
+    <h3>TECH-BASE参加にあたり、意気込み</h3>
+    チームワークを大切にし、お互いの知識やノウハウを共有することでより良い作品を作れるよう頑張りたいです！<br>
+    初心者ですが精一杯成し遂げたいと思います。よろしくお願いいたします！
+    </div>
+
+    <hr>
+    <a href="#midashi_1"> このページのTOPへ </a> ／ <a href="https://www.google.co.jp/" target="_blank"> ググる </a>
+
+    <hr>
+
+    <!-- 以下は投稿フォーム欄ですが、今は＆このままでは機能しておらずエラーが発生します。 -->
+    <!-- したがって今はコピペするだけでOKです。スタートアップ以降のPHPミッションを進める際に、参考にしてください。 -->
+    <form method="POST" action="">
+        　お名前：<input type="text" name="name"><br>
+        コメント：<input type="text" name="comment"><br>
+        <input type="submit" name="submit" value="送信">
+        <input type="reset" value="取消する">
+    </form>
+
+    </body>
+    </html>
